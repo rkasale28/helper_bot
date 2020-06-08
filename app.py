451909c8c08bbf720574d71b75025c572a2e39a3
rkasale28 <rohitkasale28@gmail.com>
2020-06-08 10:51:24 +0530

    Designed Login and Signup Pages

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from flask import Flask, redirect, url_for, request, render_template
 import requests
 import json
 
-app = Flask(__name__, template_folder= 'Templates')
+app = Flask(__name__)
 context_set = ""
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+  return render_template('signup.html')
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
