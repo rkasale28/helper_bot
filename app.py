e984c2c60cc0cf774b0a23cc2d44867626b1572b
rkasale28 <rohitkasale28@gmail.com>
2020-06-08 18:10:28 +0530

    Added Login and Signup Functionality

--- app.py
+++ app.py
@@ -1,30 +1,87 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, flash
+from flask_migrate import MigrateCommand
+from flask_script import Manager
+from models import db,migrate,user_datastore,Tourist,User,Role
+from flask_security.utils import hash_password,verify_password,login_user,logout_user
+from flask_security import Security,current_user,login_required
 import requests
 import json
 
-app = Flask(__name__)
-context_set = ""
+def create_app():
+  app = Flask(__name__)
+  context_set = ""
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@127.0.0.1:5432/tourist'
+  app.config['SECRET_KEY']='ROHIT'
+  app.config['SECURITY_PASSWORD_SALT']='ROHIT'
+  db.init_app(app)
+  migrate.init_app(app,db)
+  return app
 
-@app.route('/login')
-def login():
-  return render_template('login.html')
+app=create_app()
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
-@app.route('/signup')
+security = Security(app, user_datastore)
+
+@app.route('/signup', methods = ['POST', 'GET'])
 def signup():
-  return render_template('signup.html')
+  if (request.method=='POST'):
+    fname=request.form['fname']
+    lname=request.form['lname']
+    mail=request.form['mail']
+    uname=request.form['uname']
+    pwd=hash_password(request.form['pwd'])
+    role=Role.query.get_or_404(2)
 
+    user_datastore.create_user(username=uname,password=pwd,roles=[role])
+    db.session.commit()
+        
+    user = User.query.filter_by(username=uname).first()
+    newtourist=Tourist(firstname=fname, lastname=lname, email=mail, user=user)
+    db.session.add(newtourist)
+    db.session.commit()
+
+    login_user(user)
+    return redirect('/')
+  else:
+    return render_template('signup.html')
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+  if (request.method=='POST'):
+    uname=request.form['uname']
+    pwd=request.form['pwd']
+    
+    user = User.query.filter_by(username = uname).first()
+
+    if not user or not verify_password(pwd, user.password):
+      flash('Please check your login details and try again.')
+      return redirect('/login')
+    else:
+      login_user(user)
+      return redirect('/')
+  else:
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login')
+
+@login_required
 @app.route('/', methods = ['POST', 'GET'])
 def index():
-
     if request.method == 'GET':
         val = str(request.args.get('text'))
         data = json.dumps({"sender": "Rasa","message": val})
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         res = requests.post('http://localhost:5005/webhooks/rest/webhook', data= data, headers = headers)
         res = res.json()
-        print(res)
         val = res[0]['text']
         return render_template('index.html', val=val)
 
 if __name__ == '__main__':
+  manager.run()
   app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,32 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_security import SQLAlchemyUserDatastore, UserMixin, RoleMixin
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(10), unique=True)
+    password = db.Column(db.String(255))
+    tourist_id = db.Column(db.Integer,db.ForeignKey('tourist.id'))
+    active = db.Column(db.Boolean())
+    roles = db.relationship('Role', secondary=roles_users,
+            backref=db.backref('users', lazy='dynamic'))
+
+class Tourist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(25), unique=True)
+    lastname = db.Column(db.String(25), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    user=db.relationship('User',backref='tourist',uselist=False)
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
